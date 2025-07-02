662a08fdad88934ac4fc0bed02040f0fd484101f
coppinaphil <pfedele996@gmail.com>
2025-07-02 00:36:26 -0400

    yep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
+import robotsTxt from 'astro-robots-txt';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap(), robotsTxt()],
+  adapter: netlify()
+});
