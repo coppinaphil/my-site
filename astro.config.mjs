3bbd0611f631a1d8af14cc2d67e4f5265930ec2c
coppinaphil <pfedele996@gmail.com>
2025-07-02 00:50:02 -0400

    removed netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rocit.netlify.app',
   vite: {
     plugins: [tailwindcss()]
   },
